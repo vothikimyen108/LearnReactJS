abb7859e587c5f10513d6054d3f2900449a2c31e
vothikimyen108 <vothikinyen2k@gmail.com>
2021-06-22 11:08:41 +0700

    add file css for ExpenseItem.js

--- ReactjsBasic/my-app/src/components/ExpenseItem.js
+++ ReactjsBasic/my-app/src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
+
+//import css
+import "./ExpenseItem.css";
 //tạo hàm mới expense ra item h2
-function ExpenseItem () {
-    return <h2>Hello yến</h2>
+function ExpenseItem() {
+    //nó chỉ trả về một thành phần html 1 phần tử gốc
+    //nó k là html mà jxl k dùng class mà dùng className
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
