0d0763e5ecb495b6c0e15ef4af17cb2e3bbb2184
vothikimyen108 <vothikinyen2k@gmail.com>
2021-06-22 10:31:19 +0700

    create app react if

---
+++ ReactjsBasic/my-app/src/App.js
@@ -0,0 +1,18 @@
+//import vào sài
+import ExpenseItem from './components/ExpenseItem.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ExpenseItem></ExpenseItem>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactjsBasic/my-app/src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+//tạo hàm mới expense ra item h2
+function ExpenseItem () {
+    return <h2>Hello yến</h2>
+}
+export default ExpenseItem;
