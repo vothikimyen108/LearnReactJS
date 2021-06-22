5fadc7f79860af59de7350f2c99c8c4b1af79e13
vothikimyen108 <vothikinyen2k@gmail.com>
2021-06-22 12:56:41 +0700

    passing via props

--- ReactjsBasic/my-app/src/components/ExpenseItem.js
+++ ReactjsBasic/my-app/src/components/ExpenseItem.js
@@ -1,21 +1,21 @@
 //import css
 import "./ExpenseItem.css";
 //tạo hàm mới expense ra item h2
-function ExpenseItem() {
-  //khai báo các biến
-  const expenseDate = new Date(2021, 2, 3);
-  const expenseTitle = "Car Insurance ";
-  const expensePrice = 294.67;
-
+function ExpenseItem(props) {
+  //   //khai báo các biến
+  // const expenseDate = new Date(2021, 2, 3);
+  //   const expenseTitle = "Car Insurance ";
+  //   const expensePrice = 294.67;
   //nó chỉ trả về một thành phần html 1 phần tử gốc
   //nó k là html mà jxl k dùng class mà dùng className
   //dùng dấu {} để thêm các biến bên ngoài, date là một đối tượng cần chuyển về tostring
+  //dùng props để hiện thị
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
