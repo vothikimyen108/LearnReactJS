5208ab104754b6a5bfaee68276d95d7287cacd05
vothikimyen108 <vothikinyen2k@gmail.com>
2021-06-23 17:46:50 +0700

    Using One State Instead (And What's Better)_en

--- ReactjsBasic/my-app/src/components/NewExpense/ExpenseForm.js
+++ ReactjsBasic/my-app/src/components/NewExpense/ExpenseForm.js
@@ -1,35 +1,61 @@
-import React, {useState}from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-    //sủ dụng khai báo nhiều state
-    const [enterTitle,setExterTitle] = useState('');
-    const [enterAmount,setEnterAmount] = useState('');
-    const [enterDate,setExterDate] = useState('');
-    //khai báo các hàm
-    const changeHandlerTitle = (Event) => {
-       setExterTitle(Event.target.value);
-    }
-    const changeHandlerAmount = (Event) => {
-        setEnterAmount(Event.target.value);
-     }
-     const changeHandlerDate = (Event) => {
-        setExterDate(Event.target.value);
-     }
+  //sủ dụng khai báo nhiều state
+  // const [enterTitle,setExterTitle] = useState('');
+  // const [enterAmount,setEnterAmount] = useState('');
+  // const [enterDate,setExterDate] = useState('');
+  //cách khách sử dụng nhiều state
+  const [userInput, setUserInput] = useState({
+    enterTitle: "",
+    enterAmount: "",
+    enterDate: "",
+  });
+
+  //khai báo các hàm
+  const changeHandlerTitle = (Event) => {
+    setUserInput({
+      ...userInput, //sao chép lại các giá trị đầu để mấy thuoocj tính còn lại khôn mất
+      enterTitle: Event.target.value,
+    });
+  };
+  const changeHandlerAmount = (Event) => {
+    setUserInput({
+      ...userInput, //sao chép lại các giá trị đầu để mấy thuoocj tính còn lại khôn mất
+      enterAmount: Event.target.value,
+    });
+  };
+  const changeHandlerDate = (Event) => {
+    setUserInput({
+      ...userInput, //sao chép lại các giá trị đầu để mấy thuoocj tính còn lại khôn mất
+      enterDate: Event.target.value,
+    });
+  };
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={changeHandlerTitle}/>
+          <input type="text" onChange={changeHandlerTitle} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={changeHandlerAmount} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={changeHandlerAmount}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={changeHandlerDate} />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={changeHandlerDate}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
