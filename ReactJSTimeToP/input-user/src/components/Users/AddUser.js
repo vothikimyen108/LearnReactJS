2a28bc1b9a742173ab0f80bf9f31133f3be480ce
vothikimyen108 <vothikinyen2k@gmail.com>
2021-06-25 16:35:10 +0700

    add funtion error input

--- ReactJSTimeToP/input-user/src/components/Users/AddUser.js
+++ ReactJSTimeToP/input-user/src/components/Users/AddUser.js
@@ -2,22 +2,39 @@ import Card from "../UI/Card";
 import style from "./AddUser.module.css";
 import Button from "../UI/Button";
 import React, { useState } from "react";
+import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-
+  const [errornofi, setErrorNoFi] = useState({
+    title: "Messgae",
+    content: "Please enter your name or age ",
+  });
+  const [error, setError] = useState(false);
   const addUserHandler = (event) => {
     //không cho gửi form
     event.preventDefault();
     //valid cho user name age điều kiện không trống và tuổi không âm
     if (enteredAge.trim().length === 0 || enteredName.trim().length === 0) {
+      setErrorNoFi({
+        title: "Messgae",
+        content: "Please enter your name or age ",
+      });
+      setError(true);
       return;
     }
-    if (enteredAge.trim() < 1) return;
+    if (enteredAge.trim() < 1) {
+      setError(true);
+      setErrorNoFi({
+        title: "message",
+        content: "Please enter age 1 older than 1",
+      });
+      return;
+    }
     //xuất kết quả thử
     // console.log(enteredAge, enteredName);
     //truyền 2 đứa này lên hàm mẹ thông quá state onAddUser
-    props.onAddUser(enteredName,  enteredAge);;
+    props.onAddUser(enteredName, enteredAge);
     setEnteredAge("");
     setEnteredName("");
   };
@@ -29,26 +46,41 @@ const AddUser = (props) => {
   const ageHandler = (event) => {
     setEnteredAge(event.target.value);
   };
+
+  //hàm xử lí nếu người bấm ok đồng ý
+  const handlerOnConFirm = () =>{
+    setError(false);
+  }
+  //nếu có lỗi mới xuất ra thông báo
   return (
-    <Card className={style.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={userNameHandler}
-          value={enteredName}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageHandler}
-          value={enteredAge}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && (
+        <ErrorModal onConfirm= {handlerOnConFirm}
+          title={errornofi.title}
+          message={errornofi.content}
+        ></ErrorModal >
+      )}
+
+      <Card className={style.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={userNameHandler}
+            value={enteredName}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageHandler}
+            value={enteredAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 export default AddUser;
