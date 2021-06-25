7e9a014f02dff729c24a987f502bd68c5f2d9b43
vothikimyen108 <vothikinyen2k@gmail.com>
2021-06-25 11:24:19 +0700

    learn styled-components

--- ReactJSStyling/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ ReactJSStyling/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
-
+//import styled để sài
+import styled from "styled-components";
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   //khai báo biến kiểm tra giá trị nhập vào
@@ -22,13 +23,38 @@ const CourseInput = (props) => {
     }
     props.onAddGoal(enteredValue);
   };
-  //dùng stlyling thông báo nếu nhập sai 
+  //khai báo biên dùng styled tạo thẻ và định dạng style cho thẻ
+  const FormControl = styled.div`
+    margin: 0.5rem 0;
+    & label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+      color: ${(props) => (props.invalid ? "red" : "black")};
+    }
+    & input {
+      display: block;
+      width: 100%;
+      border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+      background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+      font: inherit;
+      line-height: 1.5rem;
+      padding: 0 0.25rem;
+    }
+    & input:focus {
+      outline: none;
+      background: #fad0ec;
+      border-color: #8b005d;
+    }
+  `;
+
+  //dùng stlyling thông báo nếu nhập sai
   return (
     <form onSubmit={formSubmitHandler}>
       {/* cách dùng css thêm vào class invalid */}
-      <div className={`form-control  ${!isValid ? 'invalid': ''}`}>
-        <label>Course Goal</label>
-        {/* <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+      {/* <div className={`form-control  ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label> */}
+      {/* <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
@@ -36,8 +62,13 @@ const CourseInput = (props) => {
             color: !isValid ? "red" : "black",
             background: !isValid ? "salmon" : "transparent",
           }} */}
-        <input type="text"  onChange={goalInputChangeHandler} />
-      </div>
+      {/* <input type="text" onChange={goalInputChangeHandler} />
+      </div> */}
+      {/* dùng trực tiếp */}
+      <FormControl invalid={!isValid}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
