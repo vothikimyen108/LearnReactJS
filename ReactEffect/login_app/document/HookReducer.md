e74771bbe69127d0d37af12d6539a35722373802
vothikimyen108 <vothikinyen2k@gmail.com>
2021-07-01 19:52:04 +0700

    useEffect and useReducer

--- ReactEffect/login_app/document/HookReducer.md
+++ ReactEffect/login_app/document/HookReducer.md
@@ -12,3 +12,35 @@ const [state, dispatch] = useReducer(reducer, initialArg, init);
 Một chức năng được kích hoạt tự động sau khi một hành động được gửi đi (qua sendFn ()) - nó nhận được ảnh chụp nhanh trạng thái mới nhất và sẽ trả về trạng thái mới, được cập nhật.
 **initialState**: Trạng thái ban đầu.
 **initFn**: Một chức năng để thiết lập trạng thái ban đầu theo chương trình
+
+Ví dụ: 
+
+``` js
+//tạo giá trị đàu của state
+const initialState = {count: 0};
+//hàm thay đổi state
+function reducer(state, action) {
+//nếu nhận increment thì count thay đổi khác...
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+     Count: {state.count}//gọi ra giá trị state
+    //   gọi dispatch() thay đổi type
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+    </>
+  );
+}
+```
+#### useEffect và useReducer?
--- ReactEffect/login_app/src/components/Login/Login.js
+++ ReactEffect/login_app/src/components/Login/Login.js
@@ -18,11 +18,22 @@ const emailReducer = (state, action) => {
   //nếu không có gì thì trả về đối tướng rỗng và sai
   return { value: "", isValid: false };
 };
+//khai báo hàm nhận 2 tham số state và action hàm này trà về trạng thái mới
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    //trả về đối tượng state lúc khởi tạo
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6};
+  }
+  return { value: "", isValid: false };
+};
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
   //nếu dùng dependencies. vơi mảng [] thì hàm dưới đây k hoạt động vì hàm đó đc gọi 1 lần
   //ta cần chuyền các dependencies vô để khi nó thay đổi thì hàm đó đc gọi
@@ -35,13 +46,13 @@ const Login = (props) => {
   const emailChangeHandler = (event) => {
     //nó có thể loại là user input và val bằng giá trị nhập vào
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
-    setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6,
-    );
+    // setFormIsValid(
+    //   event.target.value.includes("@") && enteredPassword.trim().length > 6,
+    // );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
     // setFormIsValid(
     //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
@@ -53,23 +64,36 @@ const Login = (props) => {
   // };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({type:'INPUT_BLUR'})
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, paswordState.value);
   };
   //leanr use reducer
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
     isvalid: false,
   });
+    //leanr use reducer
+    const [paswordState, dispatchPassword] = useReducer(passwordReducer, {
+      value: "",
+      isvalid: false,
+    });
   //kiểm tra sử lý email
   const validateEmailHandler = () => {
     dispatchEmail({type: 'INPUT_BLUR'});
   };
-
+//user reduce and effect
+//dùng cấu trúc hủy
+const {isValid:emailISValid} = emailState;
+const {isValid:passwordIsValid} = paswordState;
+  useEffect(()=>{
+    setFormIsValid(
+      emailISValid && passwordIsValid
+    );
+  },[emailISValid,passwordIsValid]);
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
@@ -89,14 +113,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            paswordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={paswordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
