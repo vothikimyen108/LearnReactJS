d434ac927aae389f2fba71321d029fd47a46afd3
vothikimyen108 <vothikinyen2k@gmail.com>
2021-07-04 17:28:07 +0700

    add update ui cart

--- ReactjsPractice/01-starting-project/src/App.js
+++ ReactjsPractice/01-starting-project/src/App.js
@@ -1,14 +1,23 @@
-import { Fragment } from "react";
+import { Fragment,useState } from "react";
 import Header from "./compoment/Layout/Hearder";
 import Meals from "./compoment/Meals/Meals";
 import "./App.css";
 import ContextCartProvice from "./Context/ContextCartProvice";
-
+import Cart from "./compoment/Cart/Cart";
 const App = () => {
+  const [openCart, setOpenCart] = useState(false);
+  const handerOnlick = () => {
+    setOpenCart(true);
+  };
+  const handerClose = () => {
+    setOpenCart(false);
+  };
   return (
     <Fragment>
-      <ContextCartProvice>
-            <Header></Header>
+   
+      <ContextCartProvice>   
+         {openCart && <Cart onClose={handerClose}></Cart>}
+            <Header onOpen={handerOnlick}></Header>
       <Meals></Meals>
       </ContextCartProvice>
   
--- ReactjsPractice/01-starting-project/src/compoment/Layout/HeaderCartButton.js
+++ ReactjsPractice/01-starting-project/src/compoment/Layout/HeaderCartButton.js
@@ -1,9 +1,11 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-
+import Cart from "../Cart/Cart";
+import React, { useState } from "react";
 const HeaderCartButton = (props) => {
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onOpen}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
