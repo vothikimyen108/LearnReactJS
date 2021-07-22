81ff83b3a93f71448059e9a322cfbc7f9c0156eb
vothikimyen108 <vothikinyen2k@gmail.com>
2021-07-22 22:26:59 +0700

    deloy app

--- ReactRouterP2/quoteapp/src/App.js
+++ ReactRouterP2/quoteapp/src/App.js
@@ -1,30 +1,44 @@
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import AllQuotes from "./Pages/AllQuote";
-import QuoteDetail from "./Pages/QuoteDetail";
-import NewQuote from "./Pages/NewQuote";
+
 import Layout from "./components/layout/Layout";
-import NotFound from "./Pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+//để nó Loanding hết mấy cái file
+const NewQuote = React.lazy(() => import("./Pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./Pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./Pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./Pages/AllQuote"));
 
 function App() {
+  //lúc load nó hiện lên ui loanding
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
